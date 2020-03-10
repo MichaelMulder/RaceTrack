f55aa18534e90a4df7d07b7cc13f33a539c7d7e2
MichaelMulder <bitstudent@bitnet.wctc.edu>
2020-03-09 21:04:24 -0500

    added batman and batmobile

---
+++ RaceTrack/Cars/Batmobile.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaceTrack.RaceTrack.Cars {
+    class Batmobile : RaceCar {
+
+        public Batmobile()
+        {
+            Name = "Batmoblie";
+            TopSpeed = 110;
+        }
+
+        public override void StartEngine()
+        {
+            Console.WriteLine($"The {Name} starts");
+        }
+
+        public override void Brake()
+        {
+            Console.WriteLine($"The {Name} is stopping!");
+            base.Brake();
+        }
+
+    }
+}
---
+++ RaceTrack/Drivers/Batman.cs
@@ -0,0 +1,23 @@
+﻿using RaceTrack.RaceTrack.Cars;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaceTrack.RaceTrack.Drivers {
+    class Batman : Driver {
+        public Batman(RaceCar car) : base(car)
+        {
+            Name = "Bruce Wayne";
+            SkillLevel = 15;
+        }
+
+        public override void Drive()
+        {
+            Car.Accelerate(SkillLevel);
+        }
+
+
+    }
+}
